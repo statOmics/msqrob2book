186258ea61dfce92826d39e6dd860c0dcba51c9d
cvanderaa <vanderaa.christophe@gmail.com>
2025-10-08 08:31:00 +0200

    Added vignettes with basic, advanced and benchmarking concepts, and compiled the book

--- utils.R
+++ utils.R
@@ -142,8 +142,12 @@ overdispersionPlot <- function(object, rowvar, modelColumn) {
 
 createPairwiseContrasts <- function(formula, coldata, var, ridge = FALSE, nullHypothesis = " = 0") {
     params <- .getParamNames(formula, coldata, var, ridge)
-    out <- combn(unique(params), 2)
-    out <- paste0(out[1, ], " - ", out[2, ])
+    out <- combn(params, 2)
+    out <- paste0(out[2, ], " - ", out[1, ])
+    hasIntercept <- attr(terms(formula), "intercept")
+    isFirst <- labels(terms(formula))[[1]] == var
+    if (hasIntercept && isFirst)
+        out <- c(out, params)
     paste0(out, nullHypothesis)
 }
 
@@ -168,7 +172,7 @@ createOneVersusAllContrasts <- function(formula, coldata, var,
     if (any(grepl("[:]", params)))
         stop("You cannot automatically create pairwise contrasts for a variable involved in an interaction.")
     if (ridge) params <- paste0("ridge", params)
-    params
+    unique(params)
 }
 
 
@@ -194,19 +198,6 @@ setMethod(
     }
 )
 
-msqrob2Results <- function(object, contrast, resultsColumnNamePrefix = "",
-                           combine = FALSE) {
-    resultCols <- .makeResultColumns(contrast, resultsColumnNamePrefix)
-    out <- lapply(resultCols, .getMsqrob2Results, object = object)
-    if (combine) {
-        for (i in names(out)) {
-            out$contrast <- i
-        }
-        out <- do.call(rbind, out)
-    }
-    out
-}
-
 .makeResultColumns <- function(contrast, resultsColumnNamePrefix) {
     if (is.null(colnames(contrast))) {
         if (resultsColumnNamePrefix == "")
@@ -225,10 +216,46 @@ msqrob2Results <- function(object, contrast, resultsColumnNamePrefix = "",
             "same 'contrast' and 'resultsColumnNamePrefix' as for ",
             "'hypothesisTest()'."
         )
-    out <- lapply(row_var, function(i) rowData(object)[[i]])
-    names(out) <- row_var
+    rowData(object)[[row_var]]
+}
+
+##' @param object A `SummarizedExperiment` object containing model
+##'    inference performed by `hypothesisTest()`.
+##' @param contrast A numeric `matrix` providing the contrast matrix
+##'     used during model estimation.
+##' @param resultsColumnNamePrefix A `character(1)` providing the
+##'     prefix used when running `hypothesisTest()`.
+##' @param combine A `logical(1)` indicating whether the result tables
+##'     should be combined in a single table (default) or return as a
+##'     list of tables. When combined, two new variables are created:
+##'     1. `contrast`tells from which contrast (column of the `contrast`
+##'     matrix) the results where obtained; 2. `feature` provides the
+##'     name of the modelled feature (this information is taken from
+##'     the rownames of the tables, but this are made unique upon
+##'     combining).
+msqrobCollect <- function(object, contrast, resultsColumnNamePrefix = "",
+                           combine = TRUE) {
+    resultCols <- .makeResultColumns(contrast, resultsColumnNamePrefix)
+    out <- lapply(resultCols, .getMsqrob2Results, object = object)
+    names(out) <- resultCols
+    if (combine) {
+        for (i in names(out)) {
+            out[[i]]$contrast <- i
+            out[[i]]$feature <- rownames(out[[i]])
+        }
+        out <- do.call(rbind, out)
+    }
     out
 }
+
+library("iSEE")
+OverdispersionPanel <- createCustomPlot(
+    function(se, rows, cols, rowvar = ".n",
+             modelColumn = "msqrobModels") {
+        overdispersionPlot(se, rowvar, modelColumn) +
+            theme_bw()
+    }, fullName = "Overdispersion plot"
+)
 
 msqrob2iSEE <- function(object, i, resultsColumnName = NULL, ...) {
     i <- QFeatures:::.normIndex(object, i)
@@ -245,12 +272,4 @@ msqrob2iSEE <- function(object, i, resultsColumnName = NULL, ...) {
         args$extra <- c(args$extra, OverdispersionPanel())
     do.call(iSEE, c(list(se), args))
 }
-
-OverdispersionPanel <- createCustomPlot(
-    function(se, rows, cols, rowvar = ".n",
-             modelColumn = "msqrobModels") {
-        overdispersionPlot(se, rowvar, modelColumn) +
-            theme_bw()
-    }, fullName = "Overdispersion plot"
-)
 
